847cf129e3220777bd9b822bac82bf1d119d4953
NathanD4vid <nathan.silva@sptech.school>
2024-05-20 14:35:36 -0300

    firt commit

---
+++ src/main/java/sptech/school/atividadecicd/AtividadeCicdApplication.java
@@ -0,0 +1,13 @@
+package sptech.school.atividadecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sptech/school/atividadecicd/AtividadeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.atividadecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
